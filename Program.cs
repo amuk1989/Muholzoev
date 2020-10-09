e2a24626e35f3ba48d187c404c068828b242cbc9
amuk1989 <58622904+amuk1989@users.noreply.github.com>
2020-10-10 03:29:43 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstHWMuholzoev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
