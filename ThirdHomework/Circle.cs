65b0c5216d44c5c6b159fb0d0dce67a3b721841b
amuk1989 <58622904+amuk1989@users.noreply.github.com>
2020-10-18 22:02:18 +0700

    Fourth

---
+++ ThirdHomework/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace FirstHWMuholzoev
+{
+    public class Circle : IFigure
+    {
+        private Arc arc;
+        private Dot dot;
+
+        public Circle(Dot center, int radius)
+        {
+            dot = new Dot(center.X + radius, center.Y);
+            arc = new Arc(center, radius, dot, dot);
+        }
+    }
+}
